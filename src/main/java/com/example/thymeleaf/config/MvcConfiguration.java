6acc802bdff571a3b13363b29db86232e8288d5b
leejinseok <sonaky47@gmail.com>
2020-01-21 11:34:41 +0900

    정적파일제공 MvcConfiguration.addResourceHandlers

--- src/main/java/com/example/thymeleaf/config/MvcConfiguration.java
+++ src/main/java/com/example/thymeleaf/config/MvcConfiguration.java
@@ -1,11 +1,14 @@
 package com.example.thymeleaf.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
-public class MvcConfiguration {
+public class MvcConfiguration implements WebMvcConfigurer {
 
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
@@ -16,5 +19,12 @@ public class MvcConfiguration {
         templateResolver.setTemplateMode("HTML5");
         templateResolver.setCacheable(false);
         return templateResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/static/**")
+            .addResourceLocations("classpath:static/");
     }
 }
